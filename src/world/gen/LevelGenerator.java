a4b927352b1328d1665a2b73ffedf86e29df7d08
luut189 <minhtuongluu74@gmail.com>
2022-12-18 19:28:22 -0500

    added more variations for the world gen

--- src/world/gen/LevelGenerator.java
+++ src/world/gen/LevelGenerator.java
@@ -13,7 +13,7 @@ public class LevelGenerator {
     }
 
     public static void noiseGenerator(double[][] worldTiles) {
-        double randomZ = Math.random();
+        double randomZ = Math.random()*10;
         for(int i = 0; i < worldTiles.length; i++) {
             for(int j = 0; j < worldTiles[i].length; j++) {
                 worldTiles[i][j] = ImprovedNoise.noise(i*0.1, j*0.1, randomZ);
