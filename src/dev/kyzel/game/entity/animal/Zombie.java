d1c7a4aca39385be8379d14c8a5c17d88648ffd9
luut189 <minhtuongluu74@gmail.com>
2023-01-16 22:57:03 -0500

    added code to create zombies in game

---
+++ src/dev/kyzel/game/entity/animal/Zombie.java
@@ -0,0 +1,64 @@
+package dev.kyzel.game.entity.animal;
+
+import java.awt.Rectangle;
+
+import dev.kyzel.game.Game;
+import dev.kyzel.game.entity.Direction;
+import dev.kyzel.game.entity.EntityState;
+import dev.kyzel.gfx.Renderer;
+import dev.kyzel.utils.AssetManager;
+
+public class Zombie extends Animal {
+
+    private boolean isLeftLeg = false;
+
+    public Zombie(Renderer render, Game game, int x, int y, int speed) {
+        super(render, game, x, y, speed);
+        hostileProb = 1;
+        maxHealthValue = 5;
+        currentHealthValue = maxHealthValue;
+        attackValue = 1;
+
+        int offsetX = render.getUnitSize()/4;
+        int offsetY = render.getUnitSize()/4;
+
+        int rectWidth = render.getUnitSize()-offsetX*2;
+        int rectHeight = render.getUnitSize()-offsetY;
+        
+        this.solidArea = new Rectangle(offsetX, offsetY, rectWidth, rectHeight);
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+    }
+
+    @Override
+    public void initTexture() {
+        animalImage = AssetManager.zombieDownImage[imageIndex];
+    }
+
+    @Override
+    public void setCurrentAnimalImage() {
+        spriteCounter++;
+        if(state == EntityState.STANDING) {
+            imageIndex = 0;
+        } else if(state == EntityState.WALKING || state == EntityState.ATTACKING) {
+            boolean isSideWay = direction == Direction.RIGHT || direction == Direction.LEFT;
+            if(spriteCounter > (isSideWay ? 10 : 12)) {
+                isLeftLeg = !isLeftLeg;
+                if(isSideWay) {
+                    imageIndex = increaseImageIndex(imageIndex, 4);
+                }
+                spriteCounter = 0;
+            }
+            if(!isSideWay) {
+                imageIndex = isLeftLeg ? 1 : 2;
+            }
+        }
+        switch(direction) {
+            case UP -> animalImage = AssetManager.zombieUpImage[imageIndex];
+            case DOWN -> animalImage = AssetManager.zombieDownImage[imageIndex];
+            case RIGHT -> animalImage = AssetManager.zombieRightImage[imageIndex];
+            case LEFT -> animalImage = AssetManager.zombieLeftImage[imageIndex];
+            default -> {}
+        }
+    }
+}
--- src/dev/kyzel/utils/EntityLoader.java
+++ src/dev/kyzel/utils/EntityLoader.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import dev.kyzel.game.Game;
 import dev.kyzel.game.entity.Entity;
 import dev.kyzel.game.entity.animal.Slime;
+import dev.kyzel.game.entity.animal.Zombie;
 import dev.kyzel.gfx.Renderer;
 import dev.kyzel.game.world.tile.Tile;
 import dev.kyzel.game.world.tile.TileManager;
@@ -12,7 +13,8 @@ import dev.kyzel.game.world.tile.TileManager;
 public class EntityLoader {
 
     public static void loadEntity(ArrayList<Entity> entityList, Renderer render, Game game, TileManager tileManager) {
-        loadSlime(10000, entityList, render, game, tileManager);
+        loadSlime(5000, entityList, render, game, tileManager);
+        loadZombie(5000, entityList, render, game, tileManager);
     }
 
     private static void loadSlime(int num, ArrayList<Entity> entityList, Renderer render, Game game, TileManager tileManager) {
@@ -30,6 +32,24 @@ public class EntityLoader {
                 currentTile = tileManager.getWorldTiles()[x][y];
             }
             entityList.add(new Slime(render, game, x*render.getUnitSize(), y*render.getUnitSize(), 1));
+        }
+    }
+
+    private static void loadZombie(int num, ArrayList<Entity> entityList, Renderer render, Game game, TileManager tileManager) {
+        for(int i = 0; i < num; i++) {
+
+            int x = (int) (Math.random() * tileManager.getMaxRow());
+            int y = (int) (Math.random() * tileManager.getMaxCol());
+            Tile currentTile = tileManager.getWorldTiles()[x][y];
+
+            while(currentTile.getTileName().equals("water") ||
+                  currentTile.isSolid()) {
+
+                x = (int) (Math.random() * tileManager.getMaxRow());
+                y = (int) (Math.random() * tileManager.getMaxCol());
+                currentTile = tileManager.getWorldTiles()[x][y];
+            }
+            entityList.add(new Zombie(render, game, x*render.getUnitSize(), y*render.getUnitSize(), 2));
         }
     }
 }
